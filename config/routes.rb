b9716a888a384f43d5bec5d5d4ddb6aa0d686126
munetomo <fishmans0120@gmail.com>
2020-09-04 10:21:28 +0900

    [add]posts/search.html.haml
    ・検索結果画面のマークアップ
    ・ユーザー情報編集画面のマークアップ
    ・CSSの細かい修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+
+  # postsコントローラ
+  resources :posts do
+    collection do
+      # searchアクションへのパス
+      get 'search'
+    end
+  end
+  # ルートパス
   root 'posts#top'
+  # トップページへのパス
   get 'posts/top', to: 'posts#top'
+  
+  # usersコントローラ
   resources :users, only: :show
 end
