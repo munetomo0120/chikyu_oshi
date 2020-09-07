aacbe8a41068d0330d1ba7a2afb8e902eafb3ed1
munetomo <fishmans0120@gmail.com>
2020-09-07 20:13:25 +0900

    [add]csv file
    ・流し込み用データ（動作確認済み）

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,36 @@ User.create!(
   [
     {
       nickname: '管理者',
+      email: 'kanri@kanri.com',
+      password: 'kanri1234',
+      id: 1
+    },
+    {
+      nickname: 'テストユーザー',
       email: 'test@test.com',
       password: 'test1234',
-      id: 1
-    }
+      id: 2
+    },
   ]
 )
 
-Post.create!(
-  [
-    {
-      name: '東京タワー',
-      country_id: 126,
-      description: '東京にあるタワーです',
-      latitude: 35.6586,
-      longitude: 139.745,
-      user_id: 1
+require "csv"
 
-    }
-  ]
+CSV.foreach('db/posts.csv') do |row|
+  Post.create(:id => row[0], :name => row[1], :country_id => row[2], :latitude => row[3], :longitude => row[4], :user_id => row[5], :description => row[6])
+end
 
-)
+# Post.create!(
+#   [
+#     {
+#       name: '東京タワー',
+#       country_id: 126,
+#       description: '東京にあるタワーです',
+#       latitude: 35.6586,
+#       longitude: 139.745,
+#       user_id: 1
+
+#     }
+#   ]
+
+# )
