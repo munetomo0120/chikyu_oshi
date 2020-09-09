be5e80cce18351fdf05914014e15e8b7a0498364
munetomo <fishmans0120@gmail.com>
2020-09-09 13:54:01 +0900

    [update]fix body when drawer opened
    ・ドロワーが開いた時に背景がスクロールしないように固定
    ・JS

--- app/assets/javascripts/drawer.js
+++ app/assets/javascripts/drawer.js
@@ -4,6 +4,13 @@ $(document).on("turbolinks:load", function(){
     $('.Toggle').click(function() {
       $(this).toggleClass('active');
       $('.menu').toggleClass('open');
+
+      if ($(this).hasClass("active")) {  // ナビを開いた時
+        $("body").addClass("is-fixed");  // 背景固定！
+      } else {                              // ナビを閉じる時
+        $("body").removeClass("is-fixed");     // 背景固定解除！
+      }
+      
     });
   });
 
